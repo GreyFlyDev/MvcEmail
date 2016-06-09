736613dce42e48d05ec7eb00e13372e09f08e3b7
Greg Flynn <gflynn@covdnssrv.co.volusia.fl.us>
2016-06-09 09:52:58 -0400

    Initial + EmailForm Model.

---
+++ Email/Email/Models/EmailForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Email.Models
+{
+    public class EmailForm
+    {
+        [Required, Display(Name="Your Name")]
+        public string FromName { get; set; }
+        [Required, Display(Name ="Your Email")]
+        public string FromEmail { get; set; }
+        [Required]
+        public string message { get; set; }
+    }
+}
